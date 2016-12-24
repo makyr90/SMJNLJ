ea124f939cb4161a438a0598ec1b5fb554c91c44
makyr90 <makyr90@gmail.com>
2016-12-24 21:42:40 +0200

    First commit

---
+++ src/main/java/aueb/msc/cs/joinalgs/App.java
@@ -0,0 +1,13 @@
+package aueb.msc.cs.joinalgs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
