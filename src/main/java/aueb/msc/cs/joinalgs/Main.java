ac1b4ef04154e8602d9cfbc6f10e389620fa8cef
makyr90 <makyr90@gmail.com>
2017-01-04 01:35:23 +0200

    Measure execution time

--- src/main/java/aueb/msc/cs/joinalgs/Main.java
+++ src/main/java/aueb/msc/cs/joinalgs/Main.java
@@ -10,30 +10,37 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		long startTime = 1, endTime = 1, elapsedTime;
 		Checkargs.checklength(args);
 		Join join = new Join(args);
-		if (join.getFile1size() + join.getFile2size() <= join.getMsize())
+		if (join.getFile1size() + join.getFile2size() <= join.getMsize()) {
+			startTime = System.nanoTime();
 			join.singlePass();
+			endTime = System.nanoTime();
 
-		else if (join.getJoinmethod().equals("NLJ")) {
+		} else if (join.getJoinmethod().equals("NLJ")) {
 			try {
+				startTime = System.nanoTime();
 				join.setNLJrockingCache(new ArrayList<String[]>());
 				join.rockingNLJ();
+				endTime = System.nanoTime();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 
 		} else
 			try {
+				startTime = System.nanoTime();
 				join.multipassSMJ();
+				endTime = System.nanoTime();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		// Test output
+		// Test output and measure execution time in milliseconds
 		ArrayList<String[]> results = ReadCSV.readFile(join.getOutput());
 		System.out.println("Joins: " + results.size());
+		elapsedTime = (endTime-startTime)/1000000;
+		System.out.println("Execution time: "+elapsedTime+" ms");
 
 	}
 }
