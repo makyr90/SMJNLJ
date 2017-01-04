34fed6763a352600041b30744d8f508e4c8a8eea
makyr90 <makyr90@gmail.com>
2017-01-04 20:05:53 +0200

    Add some more tests

--- src/test/java/aueb/msc/cs/joinalgs/MainBCTest.java
+++ src/test/java/aueb/msc/cs/joinalgs/MainBCTest.java
@@ -12,19 +12,19 @@ import org.junit.rules.TemporaryFolder;
 
 import aueb.msc.cs.utils.ReadCSV;
  
-public class MainABTest {
+public class MainBCTest {
     
 	 @Rule
      public TemporaryFolder folder= new TemporaryFolder();
 
      @Test
-     public void testMainA0B0nlj() throws IOException {
+     public void testMainB0C0nlj() throws IOException {
             
     	    File createdFile= folder.newFile("output.csv");
             ClassLoader classLoader = getClass().getClassLoader();
-            File file1 = new File(classLoader.getResource("A.csv").getFile());
-            File file2 = new File(classLoader.getResource("B.csv").getFile());
-            File dbmsResults = new File(classLoader.getResource("A0B0.csv").getFile());
+            File file1 = new File(classLoader.getResource("B.csv").getFile());
+            File file2 = new File(classLoader.getResource("C.csv").getFile());
+            File dbmsResults = new File(classLoader.getResource("B0C0.csv").getFile());
              
 			Main.main(Utilities.parseArguments(file1, file2, createdFile, "nlj", "0", "0"));
             ArrayList<String[]> programResults = ReadCSV.readFile(createdFile.getAbsolutePath(), ",");
@@ -36,13 +36,13 @@ public class MainABTest {
      }
      
      @Test
-     public void testMainA0B0smj() throws IOException {
+     public void testMainB0C0smj() throws IOException {
             
     	    File createdFile= folder.newFile("output.csv");
             ClassLoader classLoader = getClass().getClassLoader();
-            File file1 = new File(classLoader.getResource("A.csv").getFile());
-            File file2 = new File(classLoader.getResource("B.csv").getFile());
-            File dbmsResults = new File(classLoader.getResource("A0B0.csv").getFile());
+            File file1 = new File(classLoader.getResource("B.csv").getFile());
+            File file2 = new File(classLoader.getResource("C.csv").getFile());
+            File dbmsResults = new File(classLoader.getResource("B0C0.csv").getFile());
              
 			Main.main(Utilities.parseArguments(file1, file2, createdFile, "smj", "0", "0"));
             ArrayList<String[]> programResults = ReadCSV.readFile(createdFile.getAbsolutePath(), ",");
@@ -52,14 +52,15 @@ public class MainABTest {
             assertTrue("DBMS and program results are different",Utilities.equalResults(programResults, DBMSResults));
             
      }
+     
      @Test
-     public void testMainA0B1nlj() throws IOException {
+     public void testMainB0C1nlj() throws IOException {
             
     	    File createdFile= folder.newFile("output.csv");
             ClassLoader classLoader = getClass().getClassLoader();
-            File file1 = new File(classLoader.getResource("A.csv").getFile());
-            File file2 = new File(classLoader.getResource("B.csv").getFile());
-            File dbmsResults = new File(classLoader.getResource("A0B1.csv").getFile());
+            File file1 = new File(classLoader.getResource("B.csv").getFile());
+            File file2 = new File(classLoader.getResource("C.csv").getFile());
+            File dbmsResults = new File(classLoader.getResource("B0C1.csv").getFile());
              
 			Main.main(Utilities.parseArguments(file1, file2, createdFile, "nlj", "0", "1"));
             ArrayList<String[]> programResults = ReadCSV.readFile(createdFile.getAbsolutePath(), ",");
@@ -71,13 +72,13 @@ public class MainABTest {
      }
      
      @Test
-     public void testMainA0B1smj() throws IOException {
+     public void testMainB0C1smj() throws IOException {
             
     	    File createdFile= folder.newFile("output.csv");
             ClassLoader classLoader = getClass().getClassLoader();
-            File file1 = new File(classLoader.getResource("A.csv").getFile());
-            File file2 = new File(classLoader.getResource("B.csv").getFile());
-            File dbmsResults = new File(classLoader.getResource("A0B1.csv").getFile());
+            File file1 = new File(classLoader.getResource("B.csv").getFile());
+            File file2 = new File(classLoader.getResource("C.csv").getFile());
+            File dbmsResults = new File(classLoader.getResource("B0C1.csv").getFile());
              
 			Main.main(Utilities.parseArguments(file1, file2, createdFile, "smj", "0", "1"));
             ArrayList<String[]> programResults = ReadCSV.readFile(createdFile.getAbsolutePath(), ",");
@@ -87,14 +88,15 @@ public class MainABTest {
             assertTrue("DBMS and program results are different",Utilities.equalResults(programResults, DBMSResults));
             
      }
+     
      @Test
-     public void testMainA0B2nlj() throws IOException {
+     public void testMainB0C2nlj() throws IOException {
             
     	    File createdFile= folder.newFile("output.csv");
             ClassLoader classLoader = getClass().getClassLoader();
-            File file1 = new File(classLoader.getResource("A.csv").getFile());
-            File file2 = new File(classLoader.getResource("B.csv").getFile());
-            File dbmsResults = new File(classLoader.getResource("A0B2.csv").getFile());
+            File file1 = new File(classLoader.getResource("B.csv").getFile());
+            File file2 = new File(classLoader.getResource("C.csv").getFile());
+            File dbmsResults = new File(classLoader.getResource("B0C2.csv").getFile());
              
 			Main.main(Utilities.parseArguments(file1, file2, createdFile, "nlj", "0", "2"));
             ArrayList<String[]> programResults = ReadCSV.readFile(createdFile.getAbsolutePath(), ",");
@@ -106,13 +108,13 @@ public class MainABTest {
      }
      
      @Test
-     public void testMainA0B2smj() throws IOException {
+     public void testMainB0C2smj() throws IOException {
             
     	    File createdFile= folder.newFile("output.csv");
             ClassLoader classLoader = getClass().getClassLoader();
-            File file1 = new File(classLoader.getResource("A.csv").getFile());
-            File file2 = new File(classLoader.getResource("B.csv").getFile());
-            File dbmsResults = new File(classLoader.getResource("A0B2.csv").getFile());
+            File file1 = new File(classLoader.getResource("B.csv").getFile());
+            File file2 = new File(classLoader.getResource("C.csv").getFile());
+            File dbmsResults = new File(classLoader.getResource("B0C2.csv").getFile());
              
 			Main.main(Utilities.parseArguments(file1, file2, createdFile, "smj", "0", "2"));
             ArrayList<String[]> programResults = ReadCSV.readFile(createdFile.getAbsolutePath(), ",");
@@ -124,13 +126,13 @@ public class MainABTest {
      }
      
      @Test
-     public void testMainA0B3nlj() throws IOException {
+     public void testMainB0C3nlj() throws IOException {
             
     	    File createdFile= folder.newFile("output.csv");
             ClassLoader classLoader = getClass().getClassLoader();
-            File file1 = new File(classLoader.getResource("A.csv").getFile());
-            File file2 = new File(classLoader.getResource("B.csv").getFile());
-            File dbmsResults = new File(classLoader.getResource("A0B3.csv").getFile());
+            File file1 = new File(classLoader.getResource("B.csv").getFile());
+            File file2 = new File(classLoader.getResource("C.csv").getFile());
+            File dbmsResults = new File(classLoader.getResource("B0C3.csv").getFile());
              
 			Main.main(Utilities.parseArguments(file1, file2, createdFile, "nlj", "0", "3"));
             ArrayList<String[]> programResults = ReadCSV.readFile(createdFile.getAbsolutePath(), ",");
@@ -142,13 +144,13 @@ public class MainABTest {
      }
      
      @Test
-     public void testMainA0B3smj() throws IOException {
+     public void testMainB0C3smj() throws IOException {
             
     	    File createdFile= folder.newFile("output.csv");
             ClassLoader classLoader = getClass().getClassLoader();
-            File file1 = new File(classLoader.getResource("A.csv").getFile());
-            File file2 = new File(classLoader.getResource("B.csv").getFile());
-            File dbmsResults = new File(classLoader.getResource("A0B3.csv").getFile());
+            File file1 = new File(classLoader.getResource("B.csv").getFile());
+            File file2 = new File(classLoader.getResource("C.csv").getFile());
+            File dbmsResults = new File(classLoader.getResource("B0C3.csv").getFile());
              
 			Main.main(Utilities.parseArguments(file1, file2, createdFile, "smj", "0", "3"));
             ArrayList<String[]> programResults = ReadCSV.readFile(createdFile.getAbsolutePath(), ",");
@@ -158,4 +160,5 @@ public class MainABTest {
             assertTrue("DBMS and program results are different",Utilities.equalResults(programResults, DBMSResults));
             
      }
+    
 }
--- src/test/java/aueb/msc/cs/joinalgs/Utilities.java
+++ src/test/java/aueb/msc/cs/joinalgs/Utilities.java
@@ -8,9 +8,15 @@ public class Utilities {
 
 	public static boolean equalResults(ArrayList<String[]> programResults, ArrayList<String[]> DBMSResults) {
 
-		for (int i = 0; i < programResults.size(); i++) {
-			if (Arrays.equals(programResults.get(i), DBMSResults.get(i)))
-				// do nothing
+		for (String[] sa : programResults) {
+			boolean equal = false;
+			for (String[] sb : DBMSResults) {
+				if (Arrays.equals(sa, sb)) {
+					equal = true;
+					break;
+				}
+			}
+			if (equal)
 				continue;
 			else
 				return false;
@@ -19,9 +25,12 @@ public class Utilities {
 		return true;
 
 	}
-	 public static String[] parseArguments(File file1,File file2,File createdFile,String method,String col1,String col2){
-		 
-		return new String[] {"-f1",file1.getAbsolutePath(),"-a1",col1,"-f2",file2.getAbsolutePath(),"-a2",col2
-    		 ,"-j",method,"-m","200","-t",file1.getParentFile().getAbsolutePath(),"-o",createdFile.getAbsolutePath()};
-	 }
+
+	public static String[] parseArguments(File file1, File file2, File createdFile, String method, String col1,
+			String col2) {
+
+		return new String[] { "-f1", file1.getAbsolutePath(), "-a1", col1, "-f2", file2.getAbsolutePath(), "-a2", col2,
+				"-j", method, "-m", "200", "-t", file1.getParentFile().getAbsolutePath(), "-o",
+				createdFile.getAbsolutePath() };
+	}
 }
